4d8cae33629a516e8527ef5aadf1311c0ef1c3f9
語喬 郭 <kakuc0e0ig@gmail.com>
2023-11-14 18:30:23 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSIT153Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index(string? name,int? age)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "guest";
+            }
+            //return Content("<h2>Ajax 你好 !!</h2>","text/html", System.Text.Encoding.UTF8);
+            return Content($"Hello {name},{age}!!");
+        }
+    }
+}
